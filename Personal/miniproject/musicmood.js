adaa588a395db998b4fb299dbcdaa62e2fc17344
viki911 <vrvicky911@gmail.com>
2022-06-03 22:49:03 +0530

    carosel

---
+++ Personal/miniproject/musicmood.js
@@ -0,0 +1,18 @@
+$('#slide-testimonal').owlCarousel({
+	margin: 0,
+	center: true,
+	loop: true,
+	nav: true,
+	responsive: {
+	0: {
+	   items: 1
+	},
+	768: {
+	   items: 2,
+	margin: 15,
+	},
+	1000: {
+	   items: 3,
+	}
+    }
+});
