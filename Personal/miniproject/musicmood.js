d7fe51ecfbaf9df31f4e21db799a230423f8a796
viki911 <vrvicky911@gmail.com>
2022-06-06 01:16:14 +0530

    playbutton

--- Personal/miniproject/musicmood.js
+++ Personal/miniproject/musicmood.js
@@ -1,4 +1,4 @@
-$('#slide-testimonal').owlCarousel({
+$('.owl-carousel').owlCarousel({
 	margin: 0,
 	center: true,
 	loop: true,
@@ -16,3 +16,74 @@ $('#slide-testimonal').owlCarousel({
 	}
     }
 });
+
+
+var music = document.querySelector('.mysong')
+var playBtn = document.querySelector('.play')
+var seekbar = document.querySelector('.seekbar')
+var currentTime = document.querySelector('.current-time')
+var duration = document.querySelector('.duration')
+
+function handlePlay() {
+    if (music.paused) {
+        music.play();
+        playBtn.className = 'pause'
+        playBtn.innerHTML = '<ion-icon name="pause-circle-outline"></ion-icon>'
+    } else {
+        music.pause();
+        playBtn.className = 'play'
+        playBtn.innerHTML = '<ion-icon name="play-circle-outline"></ion-icon>'
+    }
+    music.addEventListener('ended', function () {
+        playBtn.className = 'play'
+        playBtn.innerHTML = '<ion-icon name="play-circle-outline"></ion-icon>'
+        music.currentTime = 0
+    });
+}
+
+// like
+var favIcon = document.querySelector('.favorite')
+function handleFavorite() {
+    favIcon.classList.toggle('active')
+    }
+
+
+
+// repeat
+var repIcon = document.querySelector('.repeat')
+function handleRepeat() {
+    if (music.loop == true) {
+        music.loop = false
+        repIcon.classList.toggle('active')
+    }
+    else {
+        music.loop = true
+        repIcon.classList.toggle('active')
+    }
+}
+
+// volume
+var volIcon = document.querySelector('.volume')
+var volBox = document.querySelector('.volume-box')
+var volumeRange = document.querySelector('.volume-range')
+var volumeDown = document.querySelector('.volume-down')
+var volumeUp = document.querySelector('.volume-up')
+
+function handleVolume() {
+    volIcon.classList.toggle('active')
+    volBox.classList.toggle('active')
+}
+
+volumeDown.addEventListener('click', handleVolumeDown);
+volumeUp.addEventListener('click', handleVolumeUp);
+
+function handleVolumeDown() {
+    volumeRange.value = Number(volumeRange.value) - 20
+    music.volume = volumeRange.value / 100
+}
+function handleVolumeUp() {
+    volumeRange.value = Number(volumeRange.value) + 20
+    music.volume = volumeRange.value / 100
+}
+
+
