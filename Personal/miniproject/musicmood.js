a1d44587e50cccc6224e74a7b5bb0a169c402019
viki911 <vrvicky911@gmail.com>
2022-06-12 16:16:07 +0530

    finish

--- Personal/miniproject/musicmood.js
+++ Personal/miniproject/musicmood.js
@@ -1,89 +1,421 @@
 $('.owl-carousel').owlCarousel({
 	margin: 0,
 	center: true,
-	loop: true,
-	nav: true,
 	responsive: {
 	0: {
 	   items: 1
 	},
-	768: {
-	   items: 2,
-	margin: 15,
-	},
-	1000: {
-	   items: 3,
-	}
     }
 });
 
-
-var music = document.querySelector('.mysong')
-var playBtn = document.querySelector('.play')
-var seekbar = document.querySelector('.seekbar')
-var currentTime = document.querySelector('.current-time')
-var duration = document.querySelector('.duration')
-
-function handlePlay() {
-    if (music.paused) {
-        music.play();
-        playBtn.className = 'pause'
-        playBtn.innerHTML = '<ion-icon name="pause-circle-outline"></ion-icon>'
-    } else {
-        music.pause();
-        playBtn.className = 'play'
-        playBtn.innerHTML = '<ion-icon name="play-circle-outline"></ion-icon>'
-    }
-    music.addEventListener('ended', function () {
-        playBtn.className = 'play'
-        playBtn.innerHTML = '<ion-icon name="play-circle-outline"></ion-icon>'
-        music.currentTime = 0
-    });
-}
-
 // like
-var favIcon = document.querySelector('.favorite')
-function handleFavorite() {
-    favIcon.classList.toggle('active')
+function handleFavorite(x) {
+     x.classList.toggle('active');
     }
-
-
 
 // repeat
-var repIcon = document.querySelector('.repeat')
-function handleRepeat() {
-    if (music.loop == true) {
-        music.loop = false
-        repIcon.classList.toggle('active')
+function handleRepeat(x) {
+        loop = true;
+        x.classList.toggle('active')
     }
+ 
+
+
+//voice assistant
+const btn = document.querySelector('.talk');
+const content = document.querySelector('.content');
+
+function speak(sentence) {
+    const text_speak = new SpeechSynthesisUtterance(sentence);
+
+    text_speak.rate = 1;
+    text_speak.pitch = 1;
+
+    window.speechSynthesis.speak(text_speak);
+}
+
+function wishMe() {
+    var day = new Date();
+    var hr = day.getHours();
+
+    if(hr >= 0 && hr < 12) {
+        speak("Good Morning Boss,What is your choice?. songs suggestion,quotes suggestion or movies suggestion");
+    }
+
+    else if(hr == 12) {
+        speak("Good noon Boss,What is your choice?. songs suggestion,quotes suggestion or movies suggestion");
+    }
+
+    else if(hr > 12 && hr <= 17) {
+        speak("Good Afternoon Boss,What is your choice?.songs suggestion ,quotes suggestion or movies suggestion");
+    }
+
     else {
-        music.loop = true
-        repIcon.classList.toggle('active')
+        speak("Good Evening Boss,What is your choice?. songs suggestion,quotes suggestion or movies suggestion");
     }
 }
+function emotionalIP(){
+	speak("What is your choice of emotion boss?")
+}
+window.addEventListener('load',()=>{
+    speak("Going online");
+    wishMe();
+	
+})
 
-// volume
-var volIcon = document.querySelector('.volume')
-var volBox = document.querySelector('.volume-box')
-var volumeRange = document.querySelector('.volume-range')
-var volumeDown = document.querySelector('.volume-down')
-var volumeUp = document.querySelector('.volume-up')
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
 
-function handleVolume() {
-    volIcon.classList.toggle('active')
-    volBox.classList.toggle('active')
+recognition.onresult = (event) => {
+    const current = event.resultIndex;
+    const transcript = event.results[current][0].transcript;
+    content.textContent = transcript;
+    speakThis(transcript.toLowerCase());
 }
 
-volumeDown.addEventListener('click', handleVolumeDown);
-volumeUp.addEventListener('click', handleVolumeUp);
+btn.addEventListener('click', ()=>{
+    recognition.start();
+})
 
-function handleVolumeDown() {
-    volumeRange.value = Number(volumeRange.value) - 20
-    music.volume = volumeRange.value / 100
+function speakThis(message) {
+    const speech = new SpeechSynthesisUtterance();
+
+    speech.text = "I did not understand what you said please try again";
+
+    if(message.includes('hey inertia') || message.includes('hello inertia')) {
+        const finalText = "Hello Boss";
+        speech.text = finalText;
+		
+    }
+
+    else if(message.includes('how are you')) {
+        const finalText = "I am fine boss tell me how can i help you";
+        speech.text = finalText;
+    }
+
+    else if(message.includes('song')) {
+		 emotionalIP();
+    }
+	else if(message.includes('quotes')) {
+		window.location.href = "/Personal/miniproject/Qutoes.html"
+		 emotionalIP();
+    }
+	else if(message.includes('movies')) {
+		window.location.href = "/Personal/miniproject/Movies.html"
+		 emotionalIP();
+    }
+    else if(message.includes('happy')) {
+		 window.location.href = "/Personal/miniproject/happy.html"
+    }else if(message.includes('sad')) {
+		 window.location.href = "/Personal/miniproject/sad.html"
+    }else if(message.includes('party')) {
+		 window.location.href = "/Personal/miniproject/party.html"
+    }else if(message.includes('motivational')) {
+		 window.location.href = "/Personal/miniproject/motivate.html"
+    }
+    else if(message.includes('open google')) {
+        window.open("https://google.com", "_blank");
+        const finalText = "Opening Google";
+        speech.text = finalText;
+    }
+    else if(message.includes('open instagram')) {
+        window.open("https://instagram.com", "_blank");
+        const finalText = "Opening instagram";
+        speech.text = finalText;
+    }
+
+    else if(message.includes('what is') || message.includes('who is') || message.includes('what are')) {
+        window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
+        const finalText = "This is what i found on internet regarding " + message;
+        speech.text = finalText;
+    }
+
+    else if(message.includes('wikipedia')) {
+        window.open(`https://en.wikipedia.org/wiki/${message.replace("wikipedia", "")}`, "_blank");
+        const finalText = "This is what i found on wikipedia regarding " + message;
+        speech.text = finalText;
+    }
+
+    else if(message.includes('time')) {
+        const time = new Date().toLocaleString(undefined, {hour: "numeric", minute: "numeric"})
+        const finalText = time;
+        speech.text = finalText;
+    }
+
+    else if(message.includes('date')) {
+        const date = new Date().toLocaleString(undefined, {month: "short", day: "numeric"})
+        const finalText = date;
+        speech.text = finalText;
+    }
+
+    else if(message.includes('calculator')) {
+        window.open('Calculator:///')
+        const finalText = "Opening Calculator";
+        speech.text = finalText;
+    }
+
+    else {
+        window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
+        const finalText = "I found some information for " + message + " on google";
+        speech.text = finalText;
+    }
+
+    speech.volume = 1;
+    speech.pitch = 1;
+    speech.rate = 1;
+
+    window.speechSynthesis.speak(speech);
 }
-function handleVolumeUp() {
-    volumeRange.value = Number(volumeRange.value) + 20
-    music.volume = volumeRange.value / 100
-}
 
 
+//play and pause
+$(function () {
+  var playerTrack = $("#player-track"),
+    bgArtwork = $("#bg-artwork"),
+    bgArtworkUrl,
+    albumName = $("#album-name"),
+    trackName = $("#track-name"),
+    albumArt = $("#album-art"),
+    sArea = $("#s-area"),
+    seekBar = $("#seek-bar"),
+    trackTime = $("#track-time"),
+    insTime = $("#ins-time"),
+    sHover = $("#s-hover"),
+    playPauseButton = $("#play-pause-button"),
+    i = playPauseButton.find("i"),
+    tProgress = $("#current-time"),
+    tTime = $("#track-length"),
+    seekT,
+    seekLoc,
+    seekBarPos,
+    cM,
+    ctMinutes,
+    ctSeconds,
+    curMinutes,
+    curSeconds,
+    durMinutes,
+    durSeconds,
+    playProgress,
+    bTime,
+    nTime = 0,
+    buffInterval = null,
+    tFlag = false,
+    albums = [
+      "Dawn",
+      "Me & You",
+      "Electro Boy",
+      "Home",
+      "Proxy (Original Mix)"
+    ],
+    trackNames = [
+      "Skylike - Dawn",
+      "Alex Skrindo - Me & You",
+      "Kaaze - Electro Boy",
+      "Jordan Schor - Home",
+      "Martin Garrix - Proxy"
+    ],
+    albumArtworks = ["_1", "_2", "_3", "_4", "_5"],
+    trackUrl = [
+      "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/2.mp3",
+      "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/1.mp3",
+      "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/3.mp3",
+      "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/4.mp3",
+      "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/5.mp3"
+    ],
+    playPreviousTrackButton = $("#play-previous"),
+    playNextTrackButton = $("#play-next"),
+    currIndex = -1;
+
+  function playPause() {
+    setTimeout(function () {
+      if (audio.paused) {
+        playerTrack.addClass("active");
+        albumArt.addClass("active");
+        checkBuffering();
+        i.attr("class", "fas fa-pause");
+        audio.play();
+      } else {
+        playerTrack.removeClass("active");
+        albumArt.removeClass("active");
+        clearInterval(buffInterval);
+        albumArt.removeClass("buffering");
+        i.attr("class", "fas fa-play");
+        audio.pause();
+      }
+    }, 300);
+  }
+
+  function showHover(event) {
+    seekBarPos = sArea.offset();
+    seekT = event.clientX - seekBarPos.left;
+    seekLoc = audio.duration * (seekT / sArea.outerWidth());
+
+    sHover.width(seekT);
+
+    cM = seekLoc / 60;
+
+    ctMinutes = Math.floor(cM);
+    ctSeconds = Math.floor(seekLoc - ctMinutes * 60);
+
+    if (ctMinutes < 0 || ctSeconds < 0) return;
+
+    if (ctMinutes < 0 || ctSeconds < 0) return;
+
+    if (ctMinutes < 10) ctMinutes = "0" + ctMinutes;
+    if (ctSeconds < 10) ctSeconds = "0" + ctSeconds;
+
+    if (isNaN(ctMinutes) || isNaN(ctSeconds)) insTime.text("--:--");
+    else insTime.text(ctMinutes + ":" + ctSeconds);
+
+    insTime.css({ left: seekT, "margin-left": "-21px" }).fadeIn(0);
+  }
+
+  function hideHover() {
+    sHover.width(0);
+    insTime.text("00:00").css({ left: "0px", "margin-left": "0px" }).fadeOut(0);
+  }
+
+  function playFromClickedPos() {
+    audio.currentTime = seekLoc;
+    seekBar.width(seekT);
+    hideHover();
+  }
+
+  function updateCurrTime() {
+    nTime = new Date();
+    nTime = nTime.getTime();
+
+    if (!tFlag) {
+      tFlag = true;
+      trackTime.addClass("active");
+    }
+
+    curMinutes = Math.floor(audio.currentTime / 60);
+    curSeconds = Math.floor(audio.currentTime - curMinutes * 60);
+
+    durMinutes = Math.floor(audio.duration / 60);
+    durSeconds = Math.floor(audio.duration - durMinutes * 60);
+
+    playProgress = (audio.currentTime / audio.duration) * 100;
+
+    if (curMinutes < 10) curMinutes = "0" + curMinutes;
+    if (curSeconds < 10) curSeconds = "0" + curSeconds;
+
+    if (durMinutes < 10) durMinutes = "0" + durMinutes;
+    if (durSeconds < 10) durSeconds = "0" + durSeconds;
+
+    if (isNaN(curMinutes) || isNaN(curSeconds)) tProgress.text("00:00");
+    else tProgress.text(curMinutes + ":" + curSeconds);
+
+    if (isNaN(durMinutes) || isNaN(durSeconds)) tTime.text("00:00");
+    else tTime.text(durMinutes + ":" + durSeconds);
+
+    if (
+      isNaN(curMinutes) ||
+      isNaN(curSeconds) ||
+      isNaN(durMinutes) ||
+      isNaN(durSeconds)
+    )
+      trackTime.removeClass("active");
+    else trackTime.addClass("active");
+
+    seekBar.width(playProgress + "%");
+
+    if (playProgress == 100) {
+      i.attr("class", "fa fa-play");
+      seekBar.width(0);
+      tProgress.text("00:00");
+      albumArt.removeClass("buffering").removeClass("active");
+      clearInterval(buffInterval);
+    }
+  }
+
+  function checkBuffering() {
+    clearInterval(buffInterval);
+    buffInterval = setInterval(function () {
+      if (nTime == 0 || bTime - nTime > 1000) albumArt.addClass("buffering");
+      else albumArt.removeClass("buffering");
+
+      bTime = new Date();
+      bTime = bTime.getTime();
+    }, 100);
+  }
+
+  function selectTrack(flag) {
+    if (flag == 0 || flag == 1) ++currIndex;
+    else --currIndex;
+
+    if (currIndex > -1 && currIndex < albumArtworks.length) {
+      if (flag == 0) i.attr("class", "fa fa-play");
+      else {
+        albumArt.removeClass("buffering");
+        i.attr("class", "fa fa-pause");
+      }
+
+      seekBar.width(0);
+      trackTime.removeClass("active");
+      tProgress.text("00:00");
+      tTime.text("00:00");
+
+      currAlbum = albums[currIndex];
+      currTrackName = trackNames[currIndex];
+      currArtwork = albumArtworks[currIndex];
+
+      audio.src = trackUrl[currIndex];
+
+      nTime = 0;
+      bTime = new Date();
+      bTime = bTime.getTime();
+
+      if (flag != 0) {
+        audio.play();
+        playerTrack.addClass("active");
+        albumArt.addClass("active");
+
+        clearInterval(buffInterval);
+        checkBuffering();
+      }
+
+      albumName.text(currAlbum);
+      trackName.text(currTrackName);
+      albumArt.find("img.active").removeClass("active");
+      $("#" + currArtwork).addClass("active");
+
+      bgArtworkUrl = $("#" + currArtwork).attr("src");
+
+      bgArtwork.css({ "background-image": "url(" + bgArtworkUrl + ")" });
+    } else {
+      if (flag == 0 || flag == 1) --currIndex;
+      else ++currIndex;
+    }
+  }
+
+  function initPlayer() {
+    audio = new Audio();
+
+    selectTrack(0);
+
+    audio.loop = false;
+
+    playPauseButton.on("click", playPause);
+
+    sArea.mousemove(function (event) {
+      showHover(event);
+    });
+
+    sArea.mouseout(hideHover);
+
+    sArea.on("click", playFromClickedPos);
+
+    $(audio).on("timeupdate", updateCurrTime);
+
+    playPreviousTrackButton.on("click", function () {
+      selectTrack(-1);
+    });
+    playNextTrackButton.on("click", function () {
+      selectTrack(1);
+    });
+  }
+
+  initPlayer();
+});
